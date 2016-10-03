b07db5cf4e04ae673c16ab10b0eb560620c0caea
James McFadden <james@jamesmcfadden.co.uk>
2016-10-04 00:29:51 +0100

    Add boss endpoints to the api index

--- app/Http/Controllers/Api/IndexController.php
+++ app/Http/Controllers/Api/IndexController.php
@@ -12,6 +12,8 @@ class IndexController
     public function index()
     {
         return [
+            'api.boss.index' => url('/api/v1/boss'),
+            'api.boss.show' => url('/api/v1/boss/{boss}'),
             'api.item.index' => url('/api/v1/item'),
             'api.item.show' => url('/api/v1/item/{item}'),
         ];
