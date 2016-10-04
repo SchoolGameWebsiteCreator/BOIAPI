5749ecd5b0fcd419972becb525c5e2b6de9a926f
James McFadden <james@jamesmcfadden.co.uk>
2016-10-04 15:16:56 +0100

    Add character endpoints to the api index

--- app/Http/Controllers/Api/IndexController.php
+++ app/Http/Controllers/Api/IndexController.php
@@ -14,6 +14,8 @@ class IndexController
         return [
             'api.boss.index' => url('/api/v1/boss'),
             'api.boss.show' => url('/api/v1/boss/{boss}'),
+            'api.character.index' => url('/api/v1/character'),
+            'api.character.show' => url('/api/v1/character/{character}'),
             'api.item.index' => url('/api/v1/item'),
             'api.item.show' => url('/api/v1/item/{item}'),
         ];
